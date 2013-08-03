80a3c0004fc4351f7bb8fc0ab1874c6cc886f887
Mukhtiar Gill <mac.gill@yahoo.co.uk>
2013-08-03 16:44:05 +0000

    removed fs fron response

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send( fs.readFile('./index.html','utf8' ) );
+  response.send("HELLO AGAIN");
 });
 
 var port = process.env.PORT || 5000;
