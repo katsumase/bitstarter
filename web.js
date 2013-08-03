28fce2550ab4bc21129b0a10a31b6c99c65b72b0
Mukhtiar Gill <mac.gill@yahoo.co.uk>
2013-08-03 16:19:32 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var fs = require('fs');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! 2');
+  response.send( fs.readFile('./index.html','utf8' ) );
 });
 
 var port = process.env.PORT || 5000;
